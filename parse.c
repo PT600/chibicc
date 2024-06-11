544dd3bd92120c6338a0037584bfb5b05aade4ee
PT600 <4975661@qq.com>
2024-06-11 11:15:17 +0800

    Add arrays of arrays

--- parse.c
+++ parse.c
@@ -120,13 +120,14 @@ static Type *func_params(Token **rest, Token *tok, Type *ty){
     return ty;
 }
 
-// type-suffix =  "(" func-params | "[" num "]"
+// type-suffix =  "(" func-params | "[" num "]" type-suffix
 static Type *type_suffix(Token **rest, Token *tok, Type *ty) {
     if (equal(tok, "(")) 
         return func_params(rest, tok->next, ty);
     if(equal(tok, "[")){
         int sz = get_number(tok->next);
-        *rest = skip(tok->next->next, "]");
+        tok = skip(tok->next->next, "]");
+        ty = type_suffix(rest, tok, ty);
         return array_of(ty, sz);
     }
     *rest = tok;
