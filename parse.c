0d3e86a368154a94d36ed10529315cfda6cc6efd
PT600 <4975661@qq.com>
2024-06-11 18:35:27 +0800

    Add sizeof

--- parse.c
+++ parse.c
@@ -465,7 +465,7 @@ static Node *funcall(Token **rest, Token *tok) {
     return node;
 }
 
-// primary = "(" expr ")" | ident func-args? | num
+// primary = "(" expr ")" | sizeof unary | ident func-args? | num
 static Node *primary(Token **rest, Token *tok) {
     if (equal(tok, "(")) {
         Node *node = expr(&tok, tok->next);
@@ -478,6 +478,13 @@ static Node *primary(Token **rest, Token *tok) {
         *rest = tok->next;
         return node;
     }
+
+    if(equal(tok, "sizeof")){
+        Node *node = unary(rest, tok->next);
+        add_type(node);
+        return new_num(node->ty->size, tok);
+    }
+
     if (tok->kind == TK_IDENT) {
         // Function call
         if (equal(tok->next, "(")) {
