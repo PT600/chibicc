f1ab9f2f4031aa70d5b1a599d146143c1744a68a
PT600 <4975661@qq.com>
2024-06-13 11:18:26 +0800

    Add \x<hexadecimal-sequence>

--- tokenize.c
+++ tokenize.c
@@ -85,6 +85,14 @@ static int read_punct(char *p) {
     return ispunct(*p) ? 1 : 0;
 }
 
+static int from_hex(char c){
+    if('0' <= c && c <= '9')
+        return c - '0';
+    if('a' <= c && c <= 'f')
+        return c - 'a' + 10;
+    return c - 'A' + 10;
+}
+
 static int read_escaped_char(char **new_pos, char* p){
     if('0' <= *p && *p <= '7'){
         // Read an octal number.
@@ -94,6 +102,18 @@ static int read_escaped_char(char **new_pos, char* p){
             if( '0' <= *p && *p <= '7')
                 c = (c << 3) + (*p++ - '0');
         }
+        *new_pos = p;
+        return c;
+    }
+
+    if(*p == 'x'){
+        // Read a hexadecimal number.
+        p++;
+        if(!isxdigit(*p))
+            error_at(p, "invalid hex escape sequence");
+        int c = 0;
+        for(; isxdigit(*p); p++)
+            c = (c<<4) + from_hex(*p);
         *new_pos = p;
         return c;
     }
